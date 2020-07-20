d5f8df1f2f2710500199082fea10815970aa6e61
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-07-20 12:35:52 +0800

    test(app): add unmount case

--- app/hooks/useKeyboardAwareState.spec.ts
+++ app/hooks/useKeyboardAwareState.spec.ts
@@ -14,16 +14,28 @@ test('Listens to the native keyboard show / hide events', () => {
 	}));
 
 	const { result } = renderedHook;
+
+	// Initial value
 	expect(result.current[0]).toEqual(false);
 
+	// Normal hooks
 	act(() => result.current[1]((value) => !value));
 	expect(result.current[0]).toEqual(true);
 
 	act(() => result.current[1]((value) => !value));
 	expect(result.current[0]).toEqual(false);
 
+	// When mounted, the state should change because the listener has been registered.
 	act(() => Keyboard.emit('keyboardDidShow'));
 	expect(result.current[0]).toEqual(true);
+
+	act(() => Keyboard.emit('keyboardDidHide'));
+	expect(result.current[0]).toEqual(false);
+
+	// When unmounted, the state shouldn't change because the listener has been deregistered.
+	renderedHook.unmount();
+	act(() => Keyboard.emit('keyboardDidShow'));
+	expect(result.current[0]).toEqual(false);
 
 	act(() => Keyboard.emit('keyboardDidHide'));
 	expect(result.current[0]).toEqual(false);
